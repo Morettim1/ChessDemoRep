7c4064af1ed79760e98494f0a53d17063b7165be
Morettim1 <maurice.moretti@capgemini.com>
2019-02-14 14:43:40 +0100

    Vierde commit.
    POST werkend met één veld (Voornaam)

--- src/main/java/com/capgemini/ChessDemo/View/ChessEndPoint.java
+++ src/main/java/com/capgemini/ChessDemo/View/ChessEndPoint.java
@@ -4,12 +4,13 @@ import com.capgemini.ChessDemo.Controller.ChessRepository;
 import com.capgemini.ChessDemo.Model.Chess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChessEndPoint
     {
-
         @Autowired
         private ChessRepository chessRepository;
 
@@ -18,6 +19,12 @@ public class ChessEndPoint
         {
             Iterable <Chess> alleSpelersOphalen = chessRepository.findAll();
             return alleSpelersOphalen;
+        }
 
+        @PostMapping("nieuwespeler")
+        public String spelerMaken(@RequestBody Chess speler)
+        {
+            chessRepository.save(speler);
+            return "het is gelukt";
         }
     }
